8b9c29f75d72020f6cd35836e069333e896d51b3
bikash <bikashmazumdar2000@gmail.com>
2016-12-11 02:37:44 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs     = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('streamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  next();
+});
+
+app.use((req,res,next) => {
+  res.render('maintenace.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    res.render('home.hbs',{
+      Title: 'Home Page',
+      welcomeMsg:'Welcome to my site'
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs',{
+      Title:'About Page',
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+      error:'Unable to find the page'
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
